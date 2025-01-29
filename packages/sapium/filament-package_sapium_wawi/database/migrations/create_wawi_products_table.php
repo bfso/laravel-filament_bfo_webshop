e8a1ffc45c3f3e4fc81cc4b26a41998e5f90d742
Dirren Levin <levin.dirren@edu.vs.ch>
2025-01-29 16:25:28 +0100

    Add Colums to wawi_products Table

--- packages/sapium/filament-package_sapium_wawi/database/migrations/create_wawi_products_table.php
+++ packages/sapium/filament-package_sapium_wawi/database/migrations/create_wawi_products_table.php
@@ -9,11 +9,30 @@ return new class extends Migration
     public function up()
     {
         Schema::create('wawi_products', function (Blueprint $table) {
-            $table->id();
-
-            // add fields
-
-            $table->timestamps();
-        });
+                $table->id();
+                $table->string('product_name');
+                $table->string('product_description');
+                $table->string('purchase_price');
+                $table->string('product_price');
+                $table->string('special_price');
+                $table->string('special_price_from');
+                $table->string('special_price_to');
+                // add fields
+    
+                $table->timestamps();
+            });
+            // Schema::create('wawi_customers', function (Blueprint $table) {
+            //     $table->id();
+            //     $table->string('customer_name');
+            //     $table->string('customer_email');
+            //     $table->string('customer_phone');
+            //     $table->string('customer_address');
+            //     $table->string('customer_country');
+            //     $table->string('isSupplier');
+            //     $table->string('isCostumer');
+            //     // add fields
+    
+            //     $table->timestamps();
+            // });
     }
 };
