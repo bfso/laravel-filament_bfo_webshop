0068a75f5898f4855e17cc08e79c5b44b2c57405
Eragonis <starkermeister@outlook.com>
2025-01-29 14:36:27 +0100

    DB shema/ WAWI

--- database/migrations/2025_01_27_150752_create_wawi_products_table.php
+++ database/migrations/2025_01_27_150752_create_wawi_products_table.php
@@ -10,7 +10,13 @@ return new class extends Migration
     {
         Schema::create('wawi_products', function (Blueprint $table) {
             $table->id();
-
+            $table->string('product_name');
+            $table->string('product_description');
+            $table->string('purchase_price');
+            $table->string('product_price');
+            $table->string('special_price');
+            $table->string('special_price_from');
+            $table->string('special_price_to');
             // add fields
 
             $table->timestamps();
