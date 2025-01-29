205504c8249d5c183489e2add3d99f22bc7f3288
Eragonis <starkermeister@outlook.com>
2025-01-29 14:58:34 +0100

    add DB customers && supplier

--- database/migrations/2025_01_27_150752_create_wawi_products_table.php
+++ database/migrations/2025_01_27_150752_create_wawi_products_table.php
@@ -21,5 +21,24 @@ return new class extends Migration
 
             $table->timestamps();
         });
+        Schema::create('wawi_customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->string('customer_email');
+            $table->string('customer_phone');
+            $table->string('customer_address');
+            $table->string('customer_country');
+            // add fields
+
+            $table->timestamps();
+        });
+        Schema::create('wawi_supplier', function (Blueprint $table) {
+            $table->id();
+            $table->string('supplier_name');
+            $table->string('supplier_email');
+            $table->string('supplier_phone');
+            $table->string('supplier_address');
+            $table->string('supplier_country');
+        });
     }
 };
