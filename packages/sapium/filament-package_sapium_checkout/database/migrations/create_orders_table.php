4f58966437d72e6830a67c3213e9573f82badae6
Leonardo Evic <evicleonardo@gmail.com>
2025-02-06 13:21:03 +0100

    add database migration for checkouts

--- packages/sapium/filament-package_sapium_checkout/database/migrations/create_orders_table.php
+++ packages/sapium/filament-package_sapium_checkout/database/migrations/create_orders_table.php
@@ -6,14 +6,57 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+
+    /**
+     * Upgrade application version
+     *
+     * @return void
+     */
     public function up()
     {
+
+        // Table: orders
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-
-            // add fields
-
+            $table->timestamp('date_time');
+            $table->decimal('end_price', 10, 2);
+            $table->integer('checkout_customer_id');
+            $table->integer('delivery_method_id');
+            $table->integer('payment_method_id');
             $table->timestamps();
+        });
+
+
+        // Table: order_items
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('original_item_id');
+            $table->string('name', 255);
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->integer('quantity');
+        });
+
+
+        // Table: checkout customers
+        Schema::create('checkout_customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 255);
+            $table->string('last_name', 255);
+            $table->string('email_address', 255);
+            $table->string('phone_number', 255);
+            $table->string('street', 255);
+            $table->string('zip', 255);
+            $table->string('city', 255);
+            $table->integer('country_id');
+            $table->timestamps();
+        });
+
+
+        // Table: checkout countries
+        Schema::create('checkout_countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('country_label');
         });
     }
 };
