49d47395678ad0d605ab4bf7301121cf8de6fc30
simonschmid07 <simon.schmid07@hotmail.com>
2025-02-06 15:36:06 +0100

    Improve formComponents

--- packages/sapium/filament-package_sapium_wawi/src/Resources/WawiProductResource.php
+++ packages/sapium/filament-package_sapium_wawi/src/Resources/WawiProductResource.php
@@ -5,6 +5,8 @@ namespace Sapium\FilamentPackageSapiumWawi\Resources;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables\Actions\DeleteBulkAction;
@@ -25,15 +27,14 @@ class WawiProductResource extends Resource
     public static function form(Form $form): Form
     {
         $formComponents = [
-            TextInput::make('id'),
             TextInput::make('product_name')->required(),
-            TextInput::make('product_description'),
+            MarkdownEditor::make('product_description')->toolbarButtons(['bold', 'italic', 'strike', 'link', 'codeBlock', 'orderedList', 'bulletList']),
             TextInput::make('purchase_price')->numeric()->suffix('CHF'),
-            TextInput::make('product_price')->required()->numeric()->suffix('CHF'),
+            TextInput::make('product_price')->gt('purchase_price')->required()->numeric()->suffix('CHF'),
             TextInput::make('special_price')->numeric()->suffix('CHF'),
             DatePicker::make('special_price_from'),
-            DatePicker::make('special_price_to'),
-
+            DatePicker::make('special_price_to')->afterOrEqual('special_price_from'),
+            FileUpload::make('image')->image()
         ];
 
         return $form->schema($formComponents);
