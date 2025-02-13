83997a2efa8c6a02bdac3f014526099851b6e1c8
RobinHood0101 <robin.kammer@edu.vs.ch>
2025-02-13 14:45:01 +0100

    update From in Checkout

--- app/Filament/Shop/Resources/CheckoutResource/Pages/CreateCheckout.php
+++ app/Filament/Shop/Resources/CheckoutResource/Pages/CreateCheckout.php
@@ -5,11 +5,14 @@ namespace App\Filament\Shop\Resources\CheckoutResource\Pages;
 use App\Filament\Shop\Resources\CheckoutResource;
 use Filament\Actions;
 use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Group;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Facades\Date;
+use Livewire\Attributes\Title;
+use Livewire\Features\SupportPageComponents\BaseTitle;
 
 class CreateCheckout extends CreateRecord
 {
@@ -20,10 +23,20 @@ class CreateCheckout extends CreateRecord
     {
         return $form
             ->schema([
-                TextInput::make('firstname')->required(),
-                TextInput::make('lastname')->required(),
-                DatePicker::make('birthdate')->required(),
-                TextInput::make('email')->email()->required(),
+                Group::make([
+                    TextInput::make('firstname')->required(),
+                    TextInput::make('lastname')->required(),
+                    DatePicker::make('birthdate')->required(),
+                    TextInput::make('email')->email()->required(),
+                    TextInput::make('phone')->required(),
+                    TextInput::make('street')->required(),
+                    TextInput::make('housenumber')->required(),
+                    TextInput::make('zip')->required(),
+                    TextInput::make('city')->required(),
+                    TextInput::make('country')->required(),
+                ])->relationship('customer'),
+                Group::make([])->relationship(' '),
+
             ]);
     }
 
