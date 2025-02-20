2b5150b07bbdc357c16fbd5fd4b8a978fdd10abf
Leonardo Evic <evicleonardo@gmail.com>
2025-02-20 13:36:08 +0100

    create all models for checkout

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -3,16 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Checkout extends Model
 {
+
     protected $fillable = [
-        
+        'date_time',
+        'end_price',
+        'checkout_customer_id',
+        'delivery_method_id',
+        'payment_method_id'
     ];
-
-
-    public function customer() : BelongsTo {
-        return $this->belongsTo(CheckoutCustomer::class);
-    }
 }
