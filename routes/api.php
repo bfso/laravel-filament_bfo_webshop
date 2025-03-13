594c3ca855aaf3c93a09e460f485128f45aceb54
zdirri <levin.dirren@edu.vs.ch>
2025-03-13 14:14:18 +0100

    Add api endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Sapium\FilamentPackageSapiumCart\Http\Controllers\CartController;
+use Sapium\FilamentPackageSapiumWawi\Controllers\ProductController;
+
+
+Route::get('/v1/products', [ProductController::class, 'index']);
 
 Route::patch('/cart/{cart}/quantity', [CartController::class, 'updateQuantity']);
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::patch('/cart/{cart}/quantity', [CartController::class, 'updateQuantity']);
+});
