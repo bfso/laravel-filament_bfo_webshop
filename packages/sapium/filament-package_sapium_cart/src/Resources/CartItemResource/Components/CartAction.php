d3dea354b3cf0960f1659a5208f6b1f6d7e41f83
Jussif79 <jussif.79@gmail.com>
2025-03-13 16:38:35 +0100

    Add Item to Cart

--- packages/sapium/filament-package_sapium_cart/src/Resources/CartItemResource/Components/CartAction.php
+++ packages/sapium/filament-package_sapium_cart/src/Resources/CartItemResource/Components/CartAction.php
@@ -5,6 +5,9 @@ namespace Sapium\FilamentPackageSapiumCart\Resources\CartItemResource\Components
 
 use Filament\Actions;
 use Filament\Tables\Actions\Action;
+use App\Models\Product;
+use App\Models\Cart;
+use Sapium\FilamentPackageSapiumCart\Models\CartItem;
 
 class CartAction extends Action
 {
@@ -12,8 +15,21 @@ class CartAction extends Action
     public function setUp(): void
     {
         $this->label('Add to Cart');
-        $this->action(function ($record) {
-            dd('asdf');
+        $this->action(function (Product $record) {
+            $cart = Cart::where('user_id',auth()->user()->id)->first();
+            if(!$cart){
+                $cart = new Cart();
+                $cart->user_id = auth()->user()->id;
+                $cart->save();
+            }
+
+            $cartItem = new CartItem();
+            $cartItem->cart_id = $cart->id;
+            $cartItem->product_id = $record->id;
+            $cartItem->save();
+
+            $cart->cartItems()->save($cartItem);
+
         });
     }
 }
