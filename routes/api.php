d32609506ec84ab3f290fd542c7a01597f9fb18e
zdirri <levin.dirren@edu.vs.ch>
2025-03-13 14:55:15 +0100

    Delete Useless code and add comment

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Sapium\FilamentPackageSapiumCart\Http\Controllers\CartController;
 use Sapium\FilamentPackageSapiumWawi\Controllers\ProductController;
 
-
-Route::get('/v1/products', [ProductController::class, 'index']);
+// Funktioniert im Moment nicht
+// Route::get('/v1/products', [ProductController::class, 'index']);
 
 Route::patch('/cart/{cart}/quantity', [CartController::class, 'updateQuantity']);
-Route::group(['prefix' => 'v1'], function () {
-    Route::get('/products', [ProductController::class, 'index']);
-    Route::patch('/cart/{cart}/quantity', [CartController::class, 'updateQuantity']);
-});
