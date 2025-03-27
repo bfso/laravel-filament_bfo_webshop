a1e2673a1a34b03f61ed61b8d78980e55b6cf3e7
Le <leokalbermatter@gmail.com>
2025-03-27 13:11:10 +0100

    attempted to fix style errors
    login and register pages don't use the same style as before and as the rest, attempting to fix it.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet"> <!-- Include custom.css -->
+    <link href="{{ asset('css/app/custom-stylesheet.css') }}" rel="stylesheet"> <!-- Include custom-stylesheet.css -->
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+            </div>
+        </nav>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
