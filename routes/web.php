f4cb236e211eccac98cc52f7672b99c28bd94e58
Leonardo Evic <evicleonardo@gmail.com>
2025-03-27 13:00:24 +0100

    complete merge

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,6 @@ use Sapium\FilamentPackageSapiumWawi\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/v1/products', [ProductController::class, 'index']);
 
-Route::get('', CheckoutConfirmationPage::class)->name('filament.pages.checkout-confirmation-page');
-
-Route::get('', CheckoutConfirmationPage::class)->name('filament.pages.checkout-confirmation-page');
 Route::get('/v1/products', [ProductController::class, 'index']);
+Route::get('', CheckoutConfirmationPage::class)->name('filament.pages.checkout-confirmation-page');
